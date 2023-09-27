24ec8d0a0f8db0eb0dd9f94301cfb7a9d444d86d
강승민 <rkdtmdals999@naver.com>
2023-09-28 00:18:17 +0900

    과제 1 코드 추가

--- chicken.c
+++ chicken.c
@@ -21,3 +21,32 @@ int main(void)
 
    return 0;
 }
+// 위 코드의 반례를 해결한 아래 코드
+// 반례: 10000 10000 1 6 3 => 12 
+// #include <stdio.h>
+
+// int main() {
+//    int A, B, C, X, Y;
+//    int sum = 0;
+//    scanf("%d%d%d%d%d", &A, &B, &C, &X, &Y);
+//    if (A + B > 2 * C) {
+//       int tmp = X < Y ? X : Y;
+//       sum = 2 * tmp * C;
+//       if (A > 2 * C) {
+//          sum += 2 * C * (X - tmp);
+//       }
+//       else {
+//          sum += A * (X - tmp);
+//       }
+//       if (B > 2 * C) {
+//          sum += 2 * C * (Y - tmp);
+//       }
+//       else {
+//          sum += B * (Y - tmp);
+//       }
+//    }
+//    else {
+//       sum = X * A + Y * B;
+//    }
+//    printf("%d\n", sum);
+// }
